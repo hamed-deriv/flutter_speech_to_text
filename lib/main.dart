b4e5d09ce17f2f74f3f83f1da05517c6c1f36116
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-04-08 11:56:05 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_speech_to_text/home_page.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Speech to Text',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(title: 'Flutter Speech to Text'),
+    );
+  }
+}
